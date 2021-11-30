1b84f9f3073fc44dff33115ca52936f48a64e696
Daniel Saldarriaga <dsaldarriaga@prevalentware.com>
2021-11-30 22:29:23 +0000

    finish federation

---
+++ index.ts
@@ -0,0 +1,22 @@
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
+import { readFileSync } from 'fs';
+
+const supergraphSdl = readFileSync('./supergraph.graphql').toString();
+
+const gateway = new ApolloGateway({
+  supergraphSdl,
+});
+
+const server = new ApolloServer({
+  gateway,
+});
+
+server
+  .listen()
+  .then(({ url }) => {
+    console.log(`🚀 Gateway ready at ${url}`);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
